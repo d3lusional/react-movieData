5dae53a805260ec987872701340b76944094db88
Del <del.larsen@gmail.com>
2018-01-28 10:10:09 -0600

    First Push

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+const movie = 'http://www.omdbapi.com/?apikey=c16b2d1d&&s='
+
+let axios = require('axios');
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      movies: '',
+
+    };
+   this.handleMovieChange = this.handleMovieChange.bind(this);
+   this.onButtonClick = this.onButtonClick.bind(this);
+
+  }
+handleMovieChange(event){
+   let movieName = this.state
+   console.log(event.target.value)
+   this.setState({movies: event.target.value})
+  }
+
+onButtonClick(event, movie){
+  console.log(event)
+  axios.get('http://www.omdbapi.com/?apikey=c16b2d1d&&s=' + movie)
+}
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Movie Data</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="row">
+          <input onChange={this.handleMovieChange} value={this.state.movie} type="text"/>
+          <button onClick={this.onButtonClick}>Search</button>
+      </div> 
+       
       </div>
     );
   }
